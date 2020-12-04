5fc6e3ddcd5cbe3ba80adf70c729e426fbd4efba
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2020-12-04 23:56:00 +0530

    added ErrorModal and LoadingSpinner component and partially integrated signup api

---
+++ src/components/ErrorModal/ErrorModal.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
+
+import Backdrop from '../Backdrop/Backdrop';
+import Button from '../Button/Button';
+
+import './ErrorModal.scss';
+
+const ErrorModal = ({ error, onCancel }) => {
+  const content = (
+    <>
+      {!!error && <Backdrop close={onCancel} />}
+      <CSSTransition
+        in={!!error}
+        timeout={200}
+        classNames="slide-in-top"
+        mountOnEnter
+        unmountOnExit
+      >
+        <div className="error-modal">
+          <div className="error-modal__header">
+            <h2>
+              An Error Occurred. Sorry, there was a problem processing your
+              request.
+            </h2>
+          </div>
+          <div className="error-modal__content">
+            <h3>{error}</h3>
+          </div>
+          <div className="error-modal__actions">
+            <Button onClick={onCancel}>OK</Button>
+          </div>
+        </div>
+      </CSSTransition>
+    </>
+  );
+
+  return ReactDOM.createPortal(content, document.getElementById('modal-hook'));
+};
+
+export default ErrorModal;
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import Input from '../Input/Input';
 import Button from '../Button/Button';
@@ -9,11 +9,16 @@ import {
 } from '../../utils/validators';
 import useForm from '../../hooks/useForm';
 import AuthContext from '../../context/AuthContext';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
+import ErrorModal from '../ErrorModal/ErrorModal';
 
 import './SignUp.scss';
 
 const SignUp = () => {
   const auth = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   const [formState, inputHandler] = useForm(
     {
       name: { value: '', isValid: false },
@@ -23,53 +28,84 @@ const SignUp = () => {
     false
   );
 
-  const signUpSubmitHandler = (e) => {
+  const signUpSubmitHandler = async (e) => {
     e.preventDefault();
-    console.log(formState);
-    auth.login();
+
+    try {
+      setIsLoading(true);
+      const response = await fetch('http://localhost:5000/users/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: formState.inputs.name.value,
+          email: formState.inputs.email.value,
+          password: formState.inputs.password.value,
+        }),
+      });
+
+      const responseData = await response.json();
+      if (!response.ok) {
+        throw new Error(responseData.message);
+      }
+      setIsLoading(false);
+      auth.login();
+    } catch (err) {
+      setIsLoading(false);
+      setError(err.message);
+    }
+  };
+
+  const errorHandler = () => {
+    setError(null);
   };
 
   return (
-    <div className="sign-up">
-      <h2>I do not have a account</h2>
-      <span>Sign up with your name, email and password</span>
-      <div className="sign-up__form">
-        <form onSubmit={signUpSubmitHandler}>
-          <Input
-            element="input"
-            id="name"
-            type="text"
-            label="Name"
-            validators={[VALIDATOR_REQUIRE()]}
-            error="Please enter a valid name."
-            onInput={inputHandler}
-          />
-          <Input
-            element="input"
-            id="email"
-            type="email"
-            label="Email"
-            autoComplete="username"
-            validators={[VALIDATOR_EMAIL()]}
-            error="Please enter a valid email."
-            onInput={inputHandler}
-          />
-          <Input
-            element="input"
-            id="password"
-            type="password"
-            label="Password"
-            autoComplete="new-password"
-            validators={[VALIDATOR_MINLENGTH(8)]}
-            error="Please enter a valid password (at least 8 characters)."
-            onInput={inputHandler}
-          />
-          <Button type="submit" disabled={!formState.isValid}>
-            SIGN UP
-          </Button>
-        </form>
+    <>
+      <ErrorModal error={error} onCancel={errorHandler} />
+      {isLoading && <LoadingSpinner />}
+      <div className="sign-up">
+        <h2>I do not have a account</h2>
+        <span>Sign up with your name, email and password</span>
+        <div className="sign-up__form">
+          <form onSubmit={signUpSubmitHandler}>
+            <Input
+              element="input"
+              id="name"
+              type="text"
+              label="Name"
+              validators={[VALIDATOR_REQUIRE()]}
+              error="Please enter a valid name."
+              onInput={inputHandler}
+            />
+            <Input
+              element="input"
+              id="email"
+              type="email"
+              label="Email"
+              autoComplete="username"
+              validators={[VALIDATOR_EMAIL()]}
+              error="Please enter a valid email."
+              onInput={inputHandler}
+            />
+            <Input
+              element="input"
+              id="password"
+              type="password"
+              label="Password"
+              autoComplete="new-password"
+              validators={[VALIDATOR_MINLENGTH(8)]}
+              error="Please enter a valid password (at least 8 characters)."
+              onInput={inputHandler}
+            />
+            <Button type="submit" disabled={!formState.isValid}>
+              SIGN UP
+            </Button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
